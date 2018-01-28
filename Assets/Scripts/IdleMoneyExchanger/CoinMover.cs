57190d710bbc90ccde94745a7e3ccbfaa3a44747
EthanLaptop <ethan.thomas.nichols@gmail.com>
2018-01-27 19:04:51 -0500

    Coins spawn properly in the play area Coins can be pulled and pushed Updated manager to allow puzzle selection

---
+++ Assets/Scripts/IdleMoneyExchanger/CoinMover.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinMover : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector2 gridPos;
+
+    public float moveTimer = .1f;
+    private float moveTimeReset;
+
+    public IdleMoneyExchangeField field;
+
+    private GameObject coinBase;
+    private int holdingCoinValue;
+    private int holdingCount;
+
+    public List<Vector2> pullDownCoins = new List<Vector2>();
+
+	// Use this for initialization
+	void Start () {
+        moveTimeReset = moveTimer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (field.gameOver) { return; }
+
+        if (pullDownCoins.Count > 0)
+        {
+            PullDown();
+            return;
+        }
+
+        MoveInput();
+	}
+
+    /// <summary>
+    /// Get player input and run the correct functions
+    /// </summary>
+    private void MoveInput()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) { PullCoins(); }
+        if (Input.GetKeyDown(KeyCode.V)) { PushCoins(); }
+
+        //Move the player, or reset the moveTimer
+        if (Input.GetKey(KeyCode.A)) { Move(Vector2.left); }
+        else if (Input.GetKey(KeyCode.D)) { Move(Vector2.right); }
+        else { moveTimer = 0; }
+
+        //Create a new row of coins at the top
+        if (Input.GetKeyDown(KeyCode.S)) { field.SpawnRow(); }
+    }
+
+    /// <summary>
+    /// Move the player in the direction passed in
+    /// </summary>
+    /// <param name="dir">The direction the player will move</param>
+    private void Move(Vector2 dir)
+    {
+        //Start the timer
+        moveTimer -= Time.deltaTime;
+
+        //Move the player when the timer is finished
+        if (moveTimer <= 0)
+        {
+            gridPos += dir;
+
+            //Keep the player within the game bounds
+            if (gridPos.x < 0) { gridPos.x = 0; }
+            if (gridPos.x >= field.width) { gridPos.x = field.width - 1; }
+
+            //Get the new position for the player
+            Vector2 newPos = field.tilePositions[gridPos];
+            newPos.y = transform.localPosition.y;
+
+            //Set the player's position, and reset the timer
+            transform.localPosition = newPos;
+            moveTimer = moveTimeReset;
+        }
+    }
+
+    private void PullCoins()
+    {
+        for (int y=0; y<field.height; y++)
+        {
+            Vector2 pos = new Vector2(gridPos.x, y);
+
+            if (field.tileValue[pos] == 0) { continue; }
+
+            if (holdingCoinValue == 0 || field.tileValue[pos] == holdingCoinValue) {
+                coinBase = Instantiate(field.coins[pos]);
+                coinBase.SetActive(false);
+                holdingCoinValue = field.tileValue[pos];
+                holdingCount++;
+
+                pullDownCoins.Add(pos);
+                field.tileValue[pos] = 0;
+
+            } else
+            {
+                return;
+            }
+        }
+    }
+
+    private void PullDown()
+    {
+        for (int i=0; i<pullDownCoins.Count; i++)
+        {
+            Vector2 curPos = pullDownCoins[i];
+
+            field.coins[curPos].transform.localPosition += Vector3.down * field.tileSize;
+
+            if (field.coins[curPos].transform.localPosition.y <= (-field.height / 2) * field.tileSize)
+            {
+                Destroy(field.coins[curPos]);
+
+                field.coins[curPos] = null;
+                field.tileValue[curPos] = 0;
+
+                pullDownCoins.RemoveAt(i);
+            }
+        }
+    }
+
+    private void PushCoins()
+    {
+        if (holdingCount > 0)
+        {
+            if (field.coins[new Vector2(gridPos.x, 0)]) { return; }
+
+            holdingCount--;
+
+            GameObject newCoin = Instantiate(coinBase);
+            newCoin.transform.SetParent(field.coinLayer.transform);
+            newCoin.transform.localPosition = field.tilePositions[new Vector2(gridPos.x, 0)];
+            newCoin.SetActive(true);
+
+            field.coins[new Vector2(gridPos.x, 0)] = newCoin;
+            
+            if (holdingCount <= 0)
+            {
+                Destroy(coinBase);
+
+                holdingCoinValue = 0;
+                coinBase = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/IdleMoneyExchanger/IdleMoneyExchangeField.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleMoneyExchangeField : PlayField {
+
+    public List<Sprite> tileSprites = new List<Sprite>();
+    public Sprite backgroundSprite;
+
+    public float newRowTimer = 4;
+    private float newRowReset;
+
+    public Sprite coinMoverSprite;
+    private GameObject coinMover;
+
+    public bool gameOver = false;
+
+    public GameObject coinLayer;
+    public Dictionary<Vector2, GameObject> coins = new Dictionary<Vector2, GameObject>();
+    public Dictionary<Vector2, int> tileValue = new Dictionary<Vector2, int>();
+
+    // Use this for initialization
+    void Start () {
+        //Temp
+        width = 7;
+        height = 12;
+
+        //Set the timer reset
+        newRowReset = newRowTimer;
+
+        //Load sprites and assets for the puzzle
+        SetTileSprites();
+        CalcTileSize(3);
+        LoadResources();
+
+        //Create the field and the buffer
+        CreateField(width, height, backgroundSprite);
+        CreateTileBuffer(width, height);
+
+        //Create an empty objects for the coins
+        coinLayer = new GameObject();
+        coinLayer.name = "Coin Layer";
+        coinLayer.transform.SetParent(transform);
+        coinLayer.transform.localPosition = Vector2.zero;
+
+        //Set all of the values and objects to 0/null
+        foreach (Vector2 pos in tilePositions.Keys)
+        {
+            tileValue[pos] = 0;
+            coins[pos] = null;
+        }
+
+        //Create the mover
+        CreateMover();
+
+        SpawnRow();
+        SpawnRow();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameOver) { return; }
+
+		if (newRowTimer <= 0 && coinMover.GetComponent<CoinMover>().pullDownCoins.Count == 0)
+        {
+            SpawnRow();
+            newRowTimer = newRowReset;
+        }
+
+        newRowTimer -= Time.deltaTime;
+
+        Ascend();
+    }
+
+    /// <summary>
+    /// Creates/load the sprites that will be used for the play area and pieces
+    /// </summary>
+    public void SetTileSprites()
+    {
+        int darkness = 1;
+        string tile = "Coin";
+
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "1", "Tarmac", darkness));
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "5", "Cheese", darkness));
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "10", "Mint", darkness));
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "50", "Spinach", darkness));
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "100", "Water", darkness));
+        tileSprites.Add(SpriteSheetManager.GetSprite(tile + "500", "Cheese", darkness));
+
+        backgroundSprite = SpriteSheetManager.GetSprite("Blank", "Lilac", darkness + 3);
+        coinMoverSprite = SpriteSheetManager.GetSprite("Blank", "Tomato", darkness + 4);
+    }
+
+    /// <summary>
+    /// Create the coin mover for the play field
+    /// </summary>
+    private void CreateMover()
+    {
+        //Get the base object
+        GameObject localObj = Resources.Load("Temp Background Tile") as GameObject;
+
+        //Instantiate the mover
+        coinMover = Instantiate(localObj);
+        coinMover.name = "Coin Mover";
+
+        //Set the size and sprite of the object
+        coinMover.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+        coinMover.GetComponent<Image>().sprite = coinMoverSprite;
+
+        //Set the parent and local position of the mover
+        coinMover.transform.SetParent(transform);
+        coinMover.transform.localPosition = tilePositions[new Vector2(width / 2, 0)] + Vector2.down * (tileSize / 2);
+
+        //Add the mover component, and get the component on the object
+        coinMover.AddComponent<CoinMover>();
+        CoinMover coinMoverComp = coinMover.GetComponent<CoinMover>();
+
+        //Set information about the mover
+        coinMoverComp.gridPos = new Vector3(width / 2, 0);
+        coinMoverComp.field = this;
+    }
+
+    /// <summary>
+    /// Spawn a row of coins at the top of the play field
+    /// </summary>
+    public void SpawnRow()
+    {
+        //Reset the timer
+        newRowTimer = newRowReset;
+
+        //Move all coins down if the coins can't spawn
+        while(tileValue[new Vector2(0, height)] != 0)
+        {
+            PushRowsDown();
+        }
+
+        //Loop through the width of the play field
+        for (int x=0; x<width; x++)
+        {
+            //Get a random coin
+            int randCoin = Random.Range(0, 6);
+
+            //Set the value of the coin
+            int coinValue = 0;
+            switch(randCoin)
+            {
+                case 0:
+                    coinValue = 1;
+                    break;
+                case 1:
+                    coinValue = 5;
+                    break;
+                case 2:
+                    coinValue = 10;
+                    break;
+                case 3:
+                    coinValue = 50;
+                    break;
+                case 4:
+                    coinValue = 100;
+                    break;
+                case 5:
+                    coinValue = 500;
+                    break;
+            }
+
+            //Get the spawn position of the coin
+            Vector2 spawnPos = new Vector2(x, height);
+
+            //Set the value of the coin value dictionary
+            tileValue[spawnPos] = coinValue;
+
+            //Spawn the coin in the right position in the play field
+            GameObject spawnCoin = Instantiate((GameObject)Resources.Load("Temp Background Tile"));
+            spawnCoin.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+            spawnCoin.GetComponent<Image>().sprite = tileSprites[randCoin];
+            spawnCoin.transform.SetParent(coinLayer.transform);
+
+            //Add the coin to the dictionary of coin objects
+            coins[spawnPos] = spawnCoin;
+        }
+    }
+
+    /// <summary>
+    /// Push all of the coins down a tile
+    /// </summary>
+    public void PushRowsDown()
+    {
+        //Loop through the play area and the buffer
+        for (int x=0; x<width; x++)
+        {
+            for (int y=0; y<(height * 2) - 1; y++)
+            {
+                //Get the position being tested
+                Vector2 pos = new Vector2(x, y);
+
+                if (y == 0 && coins[pos]) {
+                    coins[pos].transform.position += Vector3.down * tileSize;
+                    gameOver = true;
+                }
+
+                //Set the new value and coin object in the play area
+                tileValue[pos] = tileValue[pos + Vector2.up];
+                coins[pos] = coins[pos + Vector2.up];
+
+                tileValue[pos + Vector2.up] = 0;
+                coins[pos + Vector2.up] = null;
+
+                //Set the position of the object if it is on the field
+                if (coins[pos]) {
+                    coins[pos].transform.localPosition = tilePositions[pos];
+                }
+            }
+        }
+    }
+
+    public void Ascend()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector2 pos = new Vector2(x, y);
+
+                if (!coins[pos]) { continue; }
+
+                if (coins[pos + Vector2.up]) { continue; }
+
+                tileValue[pos + Vector2.up] = tileValue[pos];
+                coins[pos + Vector2.up] = coins[pos];
+
+                coins[pos + Vector2.up].transform.localPosition = tilePositions[pos + Vector2.up];
+
+                coins[pos] = null;
+                tileValue[pos] = 0;
+            }
+        }
+    }
+}
